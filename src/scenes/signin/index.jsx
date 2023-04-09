74355b6d0c2599ec180cb1c20a37a2e7098831f1
nikitaborozdin <oyway6173@gmail.com>
2023-04-09 14:21:11 +0300

    unauthorized status handle added

--- src/scenes/signin/index.jsx
+++ src/scenes/signin/index.jsx
@@ -20,38 +20,76 @@ class Login extends Form {
             window.localStorage.setItem("token", JSON.stringify(data));
             window.location = "/";
         } catch (error) {
+            console.log(error)
             const errors = { ...this.state.errors };
             errors.email = error.response.data.email;
             errors.password = error.response.data.password;
+            window.localStorage.setItem("loginError", error.request.statusText);
+            console.log(errors.message)
             this.setState({ errors });
         }
     };
+
+   
+
     render() {
-        return (
-            <form
-                onSubmit={this.handleSubmit}
-                // className={styles}
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  height: '100vh',
-                  flexDirection: 'column'
-                }}
-            >
-                <Paper elevation={3} className="form">
-                    <Typography variant="h6" >
-                    Вход
-                    </Typography>
-                    {this.renderInput("email", "Email", "email")}
-                    {this.renderInput("password", "Password", "password")}
-                    {this.renderSubmitBtn("Войти")}
-                </Paper>
-                <div style={{ margin: "10px 0" }}>
-                    Не знаете данные для входа? <Link to="/signup">Запросить </Link>
-                </div>
-            </form>
-        );
+        if (localStorage.getItem("loginError") == "UNAUTHORIZED"){
+            localStorage.removeItem("loginError");
+            return(
+                <form
+                    onSubmit={this.handleSubmit}
+                    // className={styles}
+                    style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100vh',
+                    flexDirection: 'column'
+                    }}
+                >
+                    <Paper elevation={3} className="form">
+                        <Typography variant="h6" >
+                        Вход
+                        </Typography>
+                        {this.renderInput("email", "Email", "email")}
+                        {this.renderInput("password", "Password", "password")}
+                        <Typography variant="h9" style={{ color: "#fc5133" }}>
+                        Неверный логин или пароль
+                        </Typography>
+                        {this.renderSubmitBtn("Войти")}
+                    </Paper>
+                    <div style={{ margin: "10px 0" }}>
+                        Не знаете данные для входа? <Link to="/signup">Запросить </Link>
+                    </div>
+                </form>
+            );
+        } else {
+            return (
+                <form
+                    onSubmit={this.handleSubmit}
+                    // className={styles}
+                    style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100vh',
+                    flexDirection: 'column'
+                    }}
+                >
+                    <Paper elevation={3} className="form">
+                        <Typography variant="h6" >
+                        Вход
+                        </Typography>
+                        {this.renderInput("email", "Email", "email")}
+                        {this.renderInput("password", "Password", "password")}
+                        {this.renderSubmitBtn("Войти")}
+                    </Paper>
+                    <div style={{ margin: "10px 0" }}>
+                        Не знаете данные для входа? <Link to="/signup">Запросить </Link>
+                    </div>
+                </form>
+            );
+        }
     }
 }
 
